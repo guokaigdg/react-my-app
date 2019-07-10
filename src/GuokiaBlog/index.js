167a8852464faba660e0e7a5e4c9413a5f55e01d
guokaigdg <guokaigdg@gmail.com>
2019-07-10 22:58:40 +0800

    博客

---
+++ src/GuokiaBlog/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './index.css'
+class Flexbox extends Component {
+  render() {
+  	return(
+      	<div className = 'box'> 1
+          <div className = 'head'></div>
+      	  <div className = 'content'>
+          <div>
+
+          </div>
+          </div>
+        </div>
+    )
+  }
+}
+export default Flexbox;
