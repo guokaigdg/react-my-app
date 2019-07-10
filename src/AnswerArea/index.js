c895db3aea277ef2ea573561499cd047a5e6b2e5
guokaigdg <guokaigdg@gmail.com>
2019-07-10 10:09:23 +0800

    优化答题区代码

--- src/AnswerArea/index.js
+++ src/AnswerArea/index.js
@@ -1,69 +1,76 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import _ from 'lodash'
-import CX from 'classnames' ;
+import CX from 'classnames'
 
-import './index.css';
+import './index.css'
 
-const list= ['A','B','C','D','E','F'];
-class AnswerArea extends Component{
-  constructor (props){
-    super(props);
-    this.state = {
-      isShow:false,
-      chooseOption: 'null',
-      isClick: false,
+const optionslist = ['A', 'B', 'C', 'D', 'E', 'F']
+class AnswerArea extends Component {
+  state = {
+    choosenOption: 'null',
+    isChoosenOption: false,
+  }
+
+  renderAnswerOptions(optionsCount) { // 渲染选项数目
+    return optionslist.slice(0, optionsCount).map(item => (
+      <div
+        role="button"
+        tabIndex={0}
+        key={item}
+        className={CX({
+          'answer-options': true,
+          'answer-options-on': this.state.choosenOption === item,
+          'answer-options-noclick': this.state.isChoosenOption === true || this.props.enableChoose === true,
+        })}
+        onClick={() => this.handleChoose(item)}
+      >
+        <span className="answer-content-chose-font">选择</span>
+        <span className="answer-options-letter-font">{item}</span>
+      </div>
+    ))
+  }
+
+  handleChoose(option) {
+    if (this.props.enableChoose === true) {
+      return
     }
-   }
-  renderAnswerOptions(answerCount) {   // 根据父组件传递个数渲染生成选项数目
-  return list.slice(0,answerCount).map((item, index) => 
-   <div 
-    key = {index} 
-    className = {CX({
-    'answer-options': true,
-    'answer-options-on': this.state.chooseOption === item,
-    'answer-options-noclick':this.state.isClick === true || this.props.isChooseDisable === true,
-     })}
-    onClick = {this.handleChoose.bind(this,item)}
-    >
-    <span className = 'answer-content-chose-font'>选择</span>
-    <span className = 'answer-options-letter-font'>{item}</span>
-    </div>
-  )
-}
-  handleChoose(choose){
-     if(this.props.isChooseDisable === true){
-       return
-     }
-      this.setState({
-        chooseOption: choose,
-        isClick:true,
-      })
-     this.props.onChooseClick()
+    this.setState({
+      choosenOption: option,
+      isChoosenOption: true,
+    })
+    this.props.onChoose(option)
   }
-  handleHidden(){
-    this.props.onHideClick();
+
+  handleHide() {
+    this.props.onClickHideButton()
   }
+
   render() {
     const wrapStyles = _.assign({}, this.props.style)
-    const {answerCount} = this.props; //父组件传递过来的选项个数
-  	return(
-      <div className = 'answer-border' style={wrapStyles}>
-        <div className = 'answer-title'>
-          <div className = 'answer-title-left'>
-            <span className = 'answer-title-left-font'>正在答题中...</span>
+    const { optionsCount } = this.props 
+    return (
+      <div className="answer-border" style={wrapStyles}>
+        <div className="answer-title">
+          <div className="answer-title-left">
+            <span className="answer-title-left-font">正在答题中...</span>
           </div>
-          <div className='answer-title-right'>
-             <span 
-              className='answer-title-right-font'
-              onClick = {this.handleHidden.bind(this)}
-             >
+          <div className="answer-title-right">
+            <span
+              role="button"
+              tabIndex={0}
+              className="answer-title-right-font"
+              onClick={() => this.handleHide()}
+            >
                隐藏
             </span>
           </div>
         </div>
-        <div className = 'answer-content'>
-          {this.renderAnswerOptions(answerCount)}         
+
+        <div className="answer-content">
+
+          {this.renderAnswerOptions(optionsCount)}
+
         </div>
       </div>
     )
@@ -71,16 +78,16 @@ class AnswerArea extends Component{
 }
 AnswerArea.propTypes = {
   style: PropTypes.object,
-  answerCount: PropTypes.number, //选项个数
-  onHideClick: PropTypes.func,  //隐藏操作
-  isChooseDisable:PropTypes.bool, //是否允许选项可点击
-  onChooseClick:PropTypes.func, //选中之后的操作
+  optionsCount: PropTypes.number, // 选项个数
+  onClickHideButton: PropTypes.func, // 隐藏操作
+  enableChoose: PropTypes.bool, // 是否允许选项可点击
+  onChoose: PropTypes.func, // 选中之后的操作
 }
 AnswerArea.defaultProps = {
-  style:{},
-  answerCount: 2,
-  onHideClick: _.noop,
-  isChooseDisable: false,
-  onChooseClick:_.noop,
+  style: {},
+  optionsCount: 2,
+  onClickHideButton: _.noop,
+  enableChoose: false,
+  onChoose: _.noop,
 }
-export default AnswerArea;
+export default AnswerArea
