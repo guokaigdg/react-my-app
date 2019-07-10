32f3c4674a829f8fea51d9d369ebeb30423904c1
guokaigdg <guokaigdg@gmail.com>
2019-07-10 13:53:51 +0800

    更新flex box练习

--- src/Flexbox/index.js
+++ src/Flexbox/index.js
@@ -4,31 +4,11 @@ class Flexbox extends Component {
   render() {
   	return(
         // <div className = 'answer-border-normal3'>
-      	<div className = 'answer-border-normal'>
-      	  <div className = 'answer-title'>
-            <div className = 'answer-title-left'>
-              <span>正在答题中...</span>
-            </div>
-            <div className = 'answer-title-right'>
-               <span>隐藏</span>
-            </div>
+      	<div className = 'box'>
+      	  <div className = 'box1'>
           </div>
-
-          <div className = 'answer-content'>
-              <div>
-                <div>
-                  <span >选择</span>
-                  <span >A</span>
-                </div>
-              </div>
-              <div >
-                <div >
-                  <span >选择</span>
-                  <span >B</span>
-                </div>
-              </div>
+          <div className = 'box2'>
           </div>
-
         </div>
     )
   }
